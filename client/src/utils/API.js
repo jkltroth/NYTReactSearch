069f7f293c2842a76e1ab7feabc208e5eb835c66
James Roth <jkltroth@gmail.com>
2018-02-24 15:09:55 -0500

    Basic API logic complete and returning object of results.

---
+++ client/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
+export default {
+  getArticles: function(searchCriteria) {
+    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+    authKey + "&q=" + searchCriteria);
+  }
+};
